d1230645839729e45242f64df5065549c6acf706
rsuomela <suomelariku@gmail.com>
2016-02-15 14:40:22 +0200

    first commit

---
+++ app/src/main/java/com/softwarefactory/teamalpha/serendipity/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.softwarefactory.teamalpha.serendipity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //Launches MainActivity when login button is clicked
+    public void login(View view){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+    //Launches SignupActivity when signup button is clicked
+    public void signup(View view){
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/softwarefactory/teamalpha/serendipity/SignupActivity.java
@@ -0,0 +1,22 @@
+package com.softwarefactory.teamalpha.serendipity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class SignupActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+    //When Signup button is pressed, the signup process is initiated and MainActivity started
+    public void signup(View view){
+        //TODO: Perform signup functions
+        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
