54b1992147e804204d45c9619b3cb9991c7f7f15
rsuomela <suomelariku@gmail.com>
2016-02-25 14:52:24 +0200

    Added items to loginActivity menu and dummy toasts for them

--- app/src/main/java/com/softwarefactory/teamalpha/serendipity/LoginActivity.java
+++ app/src/main/java/com/softwarefactory/teamalpha/serendipity/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageSwitcher;
+import android.widget.Toast;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -26,18 +27,26 @@ public class LoginActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //Act on the basis of which item is selected in the menu
+        switch (item.getItemId())
+        {
+            case R.id.action_settings:
+                Toast.makeText(LoginActivity.this, "Settings selected", Toast.LENGTH_SHORT).show();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.menu_help:
+                Toast.makeText(LoginActivity.this, "Help is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_about:
+                Toast.makeText(LoginActivity.this, "About is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
     //Launches MainActivity when login button is clicked
     public void login(View view){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
--- app/src/main/java/com/softwarefactory/teamalpha/serendipity/MapsActivity.java
+++ app/src/main/java/com/softwarefactory/teamalpha/serendipity/MapsActivity.java
@@ -14,7 +14,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    //Static GPS coordinates for specific places in Italy
     static final LatLng BOLZANO = new LatLng(46.4983, 11.3548);
+    static final LatLng SARENTINO = new LatLng(46.6412, 11.3541);
+    static final LatLng AUER = new LatLng(46.3481, 11.2990);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +42,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        // Here we add a few markers to various places for demo purposes
         // Add a marker in Bolzano, Italy
         Marker bolzano = mMap.addMarker(new MarkerOptions()
-                                .position(BOLZANO)
-                                .title("Soundtrack #1")
-                                .snippet("The story of this soundtrack..."));
+                .position(BOLZANO)
+                .title("Soundtrack #1")
+                .snippet("The story of this soundtrack..."));
         bolzano.showInfoWindow();
+        // Add a marker in Sarentino, Italy
+        Marker sarentino = mMap.addMarker(new MarkerOptions()
+                .position(SARENTINO)
+                .title("Soundtrack #2")
+                .snippet("The story of this soundtrack..."));
+        // Add a marker in Auer, Italy
+        Marker auer = mMap.addMarker(new MarkerOptions()
+                .position(AUER)
+                .title("Soundtrack #3")
+                .snippet("The story of this soundtrack..."));
         // Move the camera to given location and zoom in to specific level
         mMap.moveCamera(CameraUpdateFactory.newLatLng(BOLZANO));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
