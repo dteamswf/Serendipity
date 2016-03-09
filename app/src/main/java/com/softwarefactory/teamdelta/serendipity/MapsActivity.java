ed223b66e1eedffcf4323222573d834adb7cbd72
rsuomela <suomelariku@gmail.com>
2016-03-09 14:30:10 +0200

    Added INTERNET permissions

--- app/src/main/java/com/softwarefactory/teamdelta/serendipity/MapsActivity.java
+++ app/src/main/java/com/softwarefactory/teamdelta/serendipity/MapsActivity.java
@@ -29,14 +29,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+     * Here the Google maps is manipulated and configured once it is launched in the app.
+     * If the device doesn't have google services, the user is prompted to install those
+     **/
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
