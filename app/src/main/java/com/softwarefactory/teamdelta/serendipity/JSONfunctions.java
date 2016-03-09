a15e001c0d8b80b6191d2922e897f1292cc76ea5
rsuomela <suomelariku@gmail.com>
2016-03-09 14:38:34 +0200

    Created JSONfunctions class for the execution of HTTP requests with JSON

---
+++ app/src/main/java/com/softwarefactory/teamdelta/serendipity/JSONfunctions.java
@@ -0,0 +1,67 @@
+package com.softwarefactory.teamdelta.serendipity;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by rickhart on 09/03/16.
+ * Core of the code obtained from stackoverflow.com user Jeffy Lazars post
+ * http://stackoverflow.com/questions/19217835/can-an-android-app-connect-directly-to-an-online-mysql-database%22
+ */
+
+//TODO: Create a POC class and activity for this to obtain and view the JSON data. Test using https://httpbin.org/ if backend not ready.
+public class JSONfunctions {
+
+    public static JSONObject getJSONfromURL(String url) {
+        InputStream is = null;
+        String result = "";
+        JSONObject jArray = null;
+
+        // Download JSON data from URL
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+
+        } catch (Exception e) {
+            Log.e("log_tag", "Error in http connection " + e.toString());
+        }
+
+        // Convert response to string
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+        } catch (Exception e) {
+            Log.e("log_tag", "Error converting result " + e.toString());
+        }
+
+        try {
+
+            jArray = new JSONObject(result);
+        } catch (JSONException e) {
+            Log.e("log_tag", "Error parsing data " + e.toString());
+        }
+
+        return jArray;
+    }
+}
