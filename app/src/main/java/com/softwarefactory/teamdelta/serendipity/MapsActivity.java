977f76b077b4a7dd16d27ede215388cd01b6c18e
rsuomela <suomelariku@gmail.com>
2016-03-29 15:21:13 +0300

    Added functionality to locate the user by clicking a 'locate the user' button

--- app/src/main/java/com/softwarefactory/teamdelta/serendipity/MapsActivity.java
+++ app/src/main/java/com/softwarefactory/teamdelta/serendipity/MapsActivity.java
@@ -1,6 +1,10 @@
 package com.softwarefactory.teamdelta.serendipity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,6 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
     /**
      * Here the Google maps is manipulated and configured once it is launched in the app.
      * If the device doesn't have google services, the user is prompted to install those
@@ -45,6 +50,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         // Disable Maps toolbar (navigation and marker)
         mMap.getUiSettings().setMapToolbarEnabled(false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        // Here we get the user location and set the camera zoom to it
+        mMap.setMyLocationEnabled(true);
+        Location userLocation = mMap.getMyLocation();
+        LatLng myLocation = null;
+        if (userLocation != null) {
+            myLocation = new LatLng(userLocation.getLatitude(),
+                    userLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+            //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation,
+            //10));
+            // Move the camera to given location and zoom in to specific level
+            // mMap.moveCamera(CameraUpdateFactory.newLatLng(BOLZANO));
+            // mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        }
         // Here we add a few markers to various places for demo purposes, hard coded dummy data
         // Add a marker in Bolzano, Italy
         Marker bolzano = mMap.addMarker(new MarkerOptions()
@@ -62,8 +95,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(AUER)
                 .title("Soundtrack #3")
                 .snippet("The story of this soundtrack..."));
-        // Move the camera to given location and zoom in to specific level
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(BOLZANO));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+
     }
 }
